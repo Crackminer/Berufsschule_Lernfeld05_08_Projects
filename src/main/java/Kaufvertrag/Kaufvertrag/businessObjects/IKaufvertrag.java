1f232a19953b94c3ee1e8707f5891858c735e8c4
Crackminer <schrewrepatrick@gmail.com>
2023-06-13 11:42:05 +0200

    updated (I)Kaufvertrag

--- src/main/java/Kaufvertrag/Kaufvertrag/businessObjects/IKaufvertrag.java
+++ src/main/java/Kaufvertrag/Kaufvertrag/businessObjects/IKaufvertrag.java
@@ -1,5 +1,16 @@
 package Kaufvertrag.Kaufvertrag.businessObjects;
 
+/**
+ * @author Patrick Schewe
+ */
 public interface IKaufvertrag
 {
+  IVertragsPartner getVerkaeufer();
+  void setVerkaeufer(IVertragsPartner vertragsPartner);
+  IVertragsPartner getKaeufer();
+  void setKaeufer(IVertragsPartner kaeufer);
+  IWare getWare();
+  void setWare(IWare ware);
+  String getZahlungsModalitaeten();
+  void setZahlungsModalitaeten(String zahlungsModalitaeten);
 }
--- src/main/java/Kaufvertrag/Kaufvertrag/dataLayer/businessObjects/Kaufvertrag.java
+++ src/main/java/Kaufvertrag/Kaufvertrag/dataLayer/businessObjects/Kaufvertrag.java
@@ -1,5 +1,83 @@
 package Kaufvertrag.Kaufvertrag.dataLayer.businessObjects;
 
-public class Kaufvertrag
+import Kaufvertrag.Kaufvertrag.businessObjects.IKaufvertrag;
+import Kaufvertrag.Kaufvertrag.businessObjects.IVertragsPartner;
+import Kaufvertrag.Kaufvertrag.businessObjects.IWare;
+
+public class Kaufvertrag implements IKaufvertrag
 {
+  private IVertragsPartner verkaeufer;
+  private IVertragsPartner kaeufer;
+  private IWare ware;
+  private String zahlungsModalitaeten;
+
+  public Kaufvertrag(IVertragsPartner kaeufer, IVertragsPartner verkaeufer, IWare ware)
+  {
+    this.kaeufer = kaeufer;
+    this.verkaeufer = verkaeufer;
+    this.ware = ware;
+    this.zahlungsModalitaeten = "";
+  }
+
+  @Override
+  public IVertragsPartner getVerkaeufer()
+  {
+    return verkaeufer;
+  }
+
+  @Override
+  public void setVerkaeufer(IVertragsPartner vertragsPartner)
+  {
+    this.verkaeufer = verkaeufer;
+  }
+
+  @Override
+  public IVertragsPartner getKaeufer()
+  {
+    return kaeufer;
+  }
+
+  @Override
+  public void setKaeufer(IVertragsPartner kaeufer)
+  {
+    this.kaeufer = kaeufer;
+  }
+
+  @Override
+  public IWare getWare()
+  {
+    return ware;
+  }
+
+  @Override
+  public void setWare(IWare ware)
+  {
+    this.ware = ware;
+  }
+
+  @Override
+  public String getZahlungsModalitaeten()
+  {
+    return zahlungsModalitaeten;
+  }
+
+  @Override
+  public void setZahlungsModalitaeten(String zahlungsModalitaeten)
+  {
+    this.zahlungsModalitaeten = zahlungsModalitaeten;
+  }
+
+  @Override
+  public String toString()
+  {
+    return  "Kaufvertrag:\n"
+          + "\tKäufer:\n"
+          + "\t\t" + kaeufer.toString() + "\n"
+          + "\tVerkäufer:\n"
+          + "\t\t" + verkaeufer.toString() + "\n"
+          + "\tWare:\n"
+          + "\t\t" + ware.toString() + "\n"
+          + "\tZahlungsmodalität:\n"
+          + "\t\t" + zahlungsModalitaeten + "\n";
+  }
 }
