8790bba16686fcdeef0dfd23ca288cf38b0b9f17
Neele Reiners <nreiners@proton.me>
2023-06-13 11:35:24 +0200

    finish IAdress and Adress

--- src/main/java/Kaufvertrag/Kaufvertrag/dataLayer/businessObjects/Adresse.java
+++ src/main/java/Kaufvertrag/Kaufvertrag/dataLayer/businessObjects/Adresse.java
@@ -1,5 +1,61 @@
 package Kaufvertrag.Kaufvertrag.dataLayer.businessObjects;
 
-public class Adresse
-{
+import Kaufvertrag.Kaufvertrag.businessObjects.IAdresse;
+
+public class Adresse implements IAdresse{
+    private String strasse;
+    private String hausNr;
+    private String plz;
+    private String ort;
+
+    public Adresse(String strasse, String hausNr, String plz, String ort){
+        this.strasse = strasse;
+        this.hausNr = hausNr;
+        this.plz = plz;
+        this.ort = ort;
+    }
+
+    public String getStrasse() {
+        return strasse;
+    }
+
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+    }
+
+
+    public String getHausNr() {
+        return hausNr;
+    }
+
+    public void setHausNr(String hausNr) {
+        this.hausNr = hausNr;
+    }
+
+    public String getPlz() {
+        return plz;
+    }
+
+    public void setPlz(String plz) {
+        this.plz = plz;
+    }
+
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
+
+    @Override
+    public String toString() {
+        return "Adresse{" +
+                "strasse='" + strasse + '\'' +
+                ", hausNr='" + hausNr + '\'' +
+                ", plz='" + plz + '\'' +
+                ", ort='" + ort + '\'' +
+                '}';
+    }
 }
