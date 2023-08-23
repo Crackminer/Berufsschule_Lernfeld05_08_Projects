80cff5e36d15f6a2637f4c29435f41f042ccf0fa
Crackminer <87083544+Crackminer@users.noreply.github.com>
2023-08-23 05:26:51 +0200

    Adresse xml template datei fertig, kann nun so in die anderen xml-dateien reinkopiert werden. wichtig sind dabei definitiv die 2 private methoden und der filepath.

--- src/main/java/Kaufvertrag/Kaufvertrag/dataLayer/dataAccessObjects/xml/AdresseDaoXml.java
+++ src/main/java/Kaufvertrag/Kaufvertrag/dataLayer/dataAccessObjects/xml/AdresseDaoXml.java
@@ -1,59 +1,234 @@
 package Kaufvertrag.Kaufvertrag.dataLayer.dataAccessObjects.xml;
 
 import Kaufvertrag.Kaufvertrag.businessObjects.IAdresse;
+import Kaufvertrag.Kaufvertrag.dataLayer.businessObjects.Adresse;
 import Kaufvertrag.Kaufvertrag.dataLayer.dataAccessObjects.IDao;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AdresseDaoXml implements IDao<IAdresse, Long>
 {
-  /*public AdresseDaoXml()
-  {
-    DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-    DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+  /************************************************************************************/
+  /************************************************************************************/
+  /** xml sollte wie folgt aussehen:                                                 **/
+  /** xml-datei                                                                      **/
+  /** root-node adresse                                                              **/
+  /**     node id                                                                    **/
+  /**         node strasse                                                           **/
+  /**         node hausnummer                                                        **/
+  /**         node plz                                                               **/
+  /**         node ort                                                               **/
+  /**     node id2                                                                   **/
+  /**         node strasse2                                                          **/
+  /**         node hausnummer2                                                       **/
+  /**         node plz2                                                              **/
+  /**         node ort2                                                              **/
+  /************************************************************************************/
+  /************************************************************************************/
+  private static final String FILEPATH = "Berufsschule_Lernfeld05_08_Projects/src/main/java/Kaufvertrag/Kaufvertrag/XML/Adresse.xml";
 
-    Document doc = docBuilder.newDocument();
-    Element rootElement = doc.createElement("adresse");
-    doc.appendChild(rootElement);
-  }*/
+  private Document getDocument()
+  {
+    try
+    {
+      DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+      File file = new File(FILEPATH);
+      if (file.exists())
+      {
+        return docBuilder.parse(file);
+      }
+      Document doc = docBuilder.newDocument();
+      Element rootElement = doc.createElement("adresse");
+      doc.appendChild(rootElement);
+      return doc;
+    }
+    catch (SAXException | IOException | ParserConfigurationException ex)
+    {
+      System.out.println("There was an unexpected Exception in AdresseDaoXml#getDocument().");
+    }
+    return null;
+  }
 
   @Override
   public IAdresse create()
   {
+    try
+    {
+      Document doc = getDocument();
+      Element root = doc.getElementById("adresse");
+      Element nodeID = doc.createElement("id");
+      //get the id here pls.
+      nodeID.setIdAttribute("", true);
+
+      Element strasse = doc.createElement("strasse");
+      strasse.setNodeValue("");
+      nodeID.appendChild(strasse);
+
+      Element hausnummer = doc.createElement("hausnummer");
+      hausnummer.setNodeValue("");
+      nodeID.appendChild(hausnummer);
+
+      Element plz = doc.createElement("postleitzahl");
+      plz.setNodeValue("");
+      nodeID.appendChild(plz);
+
+      Element ort = doc.createElement("ort");
+      ort.setNodeValue("");
+      nodeID.appendChild(ort);
+
+      root.appendChild(nodeID);
+      Adresse adresse = new Adresse(strasse.getNodeValue(), hausnummer.getNodeValue(), plz.getNodeValue(), ort.getNodeValue());
+      writeToXML(doc, new FileOutputStream(FILEPATH));
+      return adresse;
+    }
+    catch (IOException ex)
+    {
+      System.out.println("There was an unexpected Exception in AdresseDaoXml#create().");
+    }
     return null;
   }
 
   @Override
   public IAdresse create(IAdresse objectToInsert)
   {
+    try
+    {
+      Document doc = getDocument();
+      Element root = doc.getElementById("adresse");
+      Element nodeID = doc.createElement("id");
+      //get the id here pls.
+      nodeID.setIdAttribute("", true);
+
+      Element strasse = doc.createElement("strasse");
+      strasse.setNodeValue(objectToInsert.getStrasse());
+      nodeID.appendChild(strasse);
+
+      Element hausnummer = doc.createElement("hausnummer");
+      hausnummer.setNodeValue(objectToInsert.getHausNr());
+      nodeID.appendChild(hausnummer);
+
+      Element plz = doc.createElement("postleitzahl");
+      plz.setNodeValue(objectToInsert.getPlz());
+      nodeID.appendChild(plz);
+
+      Element ort = doc.createElement("ort");
+      ort.setNodeValue(objectToInsert.getOrt());
+      nodeID.appendChild(ort);
+
+      root.appendChild(nodeID);
+      writeToXML(doc, new FileOutputStream(FILEPATH));
+      return objectToInsert;
+    }
+    catch (IOException ex)
+    {
+      System.out.println("There was an unexpected Exception in AdresseDaoXml#create(IAdresse objectToInsert).");
+    }
     return null;
   }
 
   @Override
   public IAdresse read(Long id)
   {
-    return null;
+    Document doc = getDocument();
+    Element root = doc.getElementById("adresse");
+    Element nodeID = root.getOwnerDocument().getElementById(id.toString());
+    Adresse adresse = new Adresse(nodeID.getElementsByTagName("strasse").item(0).getNodeValue(), nodeID.getElementsByTagName("hausnummer").item(0).getNodeValue(), nodeID.getElementsByTagName("postleitzahl").item(0).getNodeValue(), nodeID.getElementsByTagName("ort").item(0).getNodeValue());
+    return adresse;
   }
 
   @Override
   public List<IAdresse> readAll()
   {
-    return null;
+    Document doc = getDocument();
+    Element root = doc.getElementById("adresse");
+    List<IAdresse> adressListe = new ArrayList<>();
+    NodeList adressen = root.getElementsByTagName("id");
+    for (int i = 0; i < adressen.getLength(); i++)
+    {
+      NodeList childs = adressen.item(i).getChildNodes();
+      adressListe.add(new Adresse(childs.item(0).getNodeValue(), childs.item(1).getNodeValue(), childs.item(2).getNodeValue(), childs.item(3).getNodeValue()));
+    }
+    return adressListe;
   }
 
   @Override
   public void update(IAdresse objectToUpdate)
   {
+    try
+    {
+      Document doc = getDocument();
+      Element root = doc.getElementById("adresse");
+      //get the id here pls.
+      Element nodeID = root.getOwnerDocument().getElementById("");
 
+      Node strasse = nodeID.getElementsByTagName("strasse").item(0);
+      strasse.setNodeValue(objectToUpdate.getStrasse());
+
+      Node hausnummer = nodeID.getElementsByTagName("hausnummer").item(0);
+      hausnummer.setNodeValue(objectToUpdate.getHausNr());
+
+      Node plz = nodeID.getElementsByTagName("postleitzahl").item(0);
+      plz.setNodeValue(objectToUpdate.getPlz());
+
+      Node ort = nodeID.getElementsByTagName("ort").item(0);
+      ort.setNodeValue(objectToUpdate.getOrt());
+
+      writeToXML(doc, new FileOutputStream(FILEPATH));
+    }
+    catch (IOException ex)
+    {
+      System.out.println("There was an unexpected Exception in AdresseDaoXml#update(IAdresse objectToUpdate).");
+    }
   }
 
   @Override
   public void delete(Long id)
   {
+    try
+    {
+      Document doc = getDocument();
+      Element root = doc.getElementById("adresse");
+      Element nodeID = root.getOwnerDocument().getElementById(id.toString());
+      root.removeChild(nodeID);
+      writeToXML(doc, new FileOutputStream(FILEPATH));
+    }
+      catch (IOException ex)
+    {
+      System.out.println("There was an unexpected Exception in AdresseDaoXml#delete(Long id).");
+    }
+  }
 
+  private void writeToXML(Document doc, OutputStream output)
+  {
+    try
+    {
+      TransformerFactory transformerFactory = TransformerFactory.newInstance();
+      Transformer transformer = transformerFactory.newTransformer();
+
+      transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+      DOMSource source = new DOMSource(doc);
+      StreamResult result = new StreamResult(output);
+
+      transformer.transform(source, result);
+    }
+    catch (TransformerException ex)
+    {
+      System.out.println("There was an unexpected Exception in AdresseDaoXml#writeToXML(Document doc, OutputStream output).");
+    }
   }
 }
