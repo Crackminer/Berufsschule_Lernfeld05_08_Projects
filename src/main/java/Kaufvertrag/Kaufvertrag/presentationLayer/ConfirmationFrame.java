dc4d61f0dddc90dd4955352ab6b6a3adbd681af8
Finja1048 <finja.danisch@gmail.com>
2023-08-30 08:47:28 +0200

    Pup-up finished

--- src/main/java/Kaufvertrag/Kaufvertrag/presentationLayer/ConfirmationFrame.java
+++ src/main/java/Kaufvertrag/Kaufvertrag/presentationLayer/ConfirmationFrame.java
@@ -1,18 +1,33 @@
 package Kaufvertrag.Kaufvertrag.presentationLayer;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ConfirmationFrame extends JFrame {
     public static void confirm() {
-        JFrame confirmFrame = new JFrame();
-        int result = JOptionPane.showConfirmDialog(confirmFrame, "Your contract has been saved and you can now go back to the main page.");
-        if (result == 0) {
-            System.out.println("Ok");
-            System.exit(0);
-        } else if (result == 1) {
-            System.out.println("You pressed NO");
-        } else {
-            System.out.println("You pressed Cancel");
-        }
+        JFrame jFrame = new JFrame();
+
+        JDialog jd = new JDialog(jFrame);
+
+        jd.setLayout(new FlowLayout());
+
+        jd.setBounds(400, 300, 450, 150);
+
+        JLabel jLabel = new JLabel("Your contract has been saved and you can now go back to the main page.");
+
+        JButton jButton = new JButton("Ok");
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jd.setVisible(false);
+                System.exit(0);
+            }
+        });
+
+        jd.add(jLabel);
+        jd.add(jButton);
+        jd.setVisible(true);
     }
 }
