0b5296397a2bc4bdbf89295dce3824ccd08e173e
christhomas <chris.thomas8898@gmail.com>
2023-09-11 11:12:50 +0200

    Updated VertragspartnerDao SQLite & XML

--- src/main/java/Kaufvertrag/Kaufvertrag/dataLayer/dataAccessObjects/sqlite/VertragspartnerDaoSqlite.java
+++ src/main/java/Kaufvertrag/Kaufvertrag/dataLayer/dataAccessObjects/sqlite/VertragspartnerDaoSqlite.java
@@ -1,8 +1,16 @@
 package Kaufvertrag.Kaufvertrag.dataLayer.dataAccessObjects.sqlite;
 
+import Kaufvertrag.Kaufvertrag.businessObjects.IAdresse;
 import Kaufvertrag.Kaufvertrag.businessObjects.IVertragspartner;
+import Kaufvertrag.Kaufvertrag.dataLayer.businessObjects.Adresse;
+import Kaufvertrag.Kaufvertrag.dataLayer.businessObjects.Vertragspartner;
 import Kaufvertrag.Kaufvertrag.dataLayer.dataAccessObjects.IDao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class VertragspartnerDaoSqlite implements IDao<IVertragspartner, String>
@@ -10,36 +18,125 @@ public class VertragspartnerDaoSqlite implements IDao<IVertragspartner, String>
   @Override
   public IVertragspartner create()
   {
-    return null;
+    Vertragspartner objectToInsert = new Vertragspartner("","");
+    try
+    {
+      Connection connection = ConnectionManager.getNewConnection();
+      String query = "INSERT into vertragspartner (vorname, nachname, ausweisNr) values (?, ? , ?)";
+      PreparedStatement statement = connection.prepareStatement(query);
+      statement.setString(1, objectToInsert.getVorname());
+      statement.setString(2, objectToInsert.getNachname());
+      statement.setString(3, objectToInsert.getAusweisNr());
+      statement.executeUpdate();
+    }
+    catch (Exception ex)
+    {
+      System.out.println("There was an unexpected Exception in VertragspartnerDaoSqlite#create(IVertragspartner objectToInsert).");
+    }
+    return objectToInsert;
   }
 
   @Override
   public void create(IVertragspartner objectToInsert)
   {
-
+    try
+    {
+      Connection connection = ConnectionManager.getNewConnection();
+      String query = "INSERT into vertragspartner (vorname, nachname, ausweisNr) values (?, ? , ?)";
+      PreparedStatement statement = connection.prepareStatement(query);
+      statement.setString(1, objectToInsert.getVorname());
+      statement.setString(2, objectToInsert.getNachname());
+      statement.setString(3, objectToInsert.getAusweisNr());
+      statement.executeUpdate();
+    }
+    catch (Exception ex)
+    {
+      System.out.println("There was an unexpected Exception in VertragspartnerDaoSqlite#create(IVertragspartner objectToInsert).");
+    }
   }
 
   @Override
   public IVertragspartner read(String id)
   {
+    try
+    {
+      Connection connection = ConnectionManager.getNewConnection();
+      String query = "SELECT vorname, nachname, ausweisNr from vertragspartner WHERE id = ?";
+      PreparedStatement statement = connection.prepareStatement(query);
+      statement.setString(1, id);
+      ResultSet result = statement.executeQuery();
+      String vorname = result.getString("vorname");
+      String nachname = result.getString("nachname");
+      String ausweisNr = result.getString("ausweisNr");
+      Vertragspartner vertragspartner = new Vertragspartner(vorname, nachname);
+      vertragspartner.setAusweisNr(ausweisNr);
+      return vertragspartner;
+    }
+    catch (Exception ex)
+    {
+      System.out.println("There was an unexpected Exception in VertragspartnerDaoSqlite#read(String id).");
+    }
     return null;
   }
 
   @Override
   public List<IVertragspartner> readAll()
   {
+    try
+    {
+      Connection connection = ConnectionManager.getNewConnection();
+      String query = "SELECT vorname, nachname, ausweisNr from vertragspartner";
+      PreparedStatement statement = connection.prepareStatement(query);
+      ResultSet result = statement.executeQuery();
+      ArrayList<IVertragspartner> list = new ArrayList<>();
+      while (result.next())
+      {
+        Vertragspartner vertragspartner = new Vertragspartner("","");
+        vertragspartner.setVorname(result.getString("vorname"));
+        vertragspartner.setNachname(result.getString("nachname"));
+        vertragspartner.setAusweisNr(result.getString("ausweisNr"));
+        list.add(vertragspartner);
+      }
+      return list;
+    }
+    catch (Exception ex)
+    {
+      System.out.println("There was an unexpected Exception in VertargspartnerDaoSqlite#readAll()");
+    }
     return null;
   }
 
   @Override
   public void update(IVertragspartner objectToUpdate)
   {
-
+    try
+    {
+      Connection connection = ConnectionManager.getNewConnection();
+      String query = "UPDATE vertragspartner SET vorname = ?, nachname = ?, ausweisNr = ? WHERE id = ?";
+      PreparedStatement statement = connection.prepareStatement(query);
+      statement.setString(1, objectToUpdate.getVorname());
+      statement.setString(2, objectToUpdate.getNachname());
+      statement.setString(3, objectToUpdate.getAusweisNr());
+      statement.executeUpdate();
+    } catch (Exception ex) {
+      System.out.println("here was an unexpected Exception in VertragspartnerDaoSqlite#update(Vertargspartner objectToUpdate).");
+    }
   }
 
   @Override
   public void delete(String id)
   {
-
+    try
+    {
+      Connection connection = ConnectionManager.getNewConnection();
+      String query = "DELETE from vertragspartner WHERE id = ?";
+      PreparedStatement statement = connection.prepareStatement(query);
+      statement.setString(1, id);
+      ResultSet result = statement.executeQuery();
+    }
+    catch (Exception ex)
+    {
+      System.out.println("There was an unexpected Exception in VertargspartnerDaoSqlite#delete(String id).");
+    }
   }
 }
--- src/main/java/Kaufvertrag/Kaufvertrag/dataLayer/dataAccessObjects/xml/VertragspartnerDaoXml.java
+++ src/main/java/Kaufvertrag/Kaufvertrag/dataLayer/dataAccessObjects/xml/VertragspartnerDaoXml.java
@@ -2,44 +2,164 @@ package Kaufvertrag.Kaufvertrag.dataLayer.dataAccessObjects.xml;
 
 import Kaufvertrag.Kaufvertrag.dataLayer.businessObjects.Vertragspartner;
 import Kaufvertrag.Kaufvertrag.dataLayer.dataAccessObjects.IDao;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import static Kaufvertrag.Kaufvertrag.dataLayer.dataAccessObjects.xml.XMLManager.getDocument;
+import static Kaufvertrag.Kaufvertrag.dataLayer.dataAccessObjects.xml.XMLManager.writeToXML;
 
 public class VertragspartnerDaoXml implements IDao<Vertragspartner, String>
 {
+  private static final String FILEPATH = "Berufsschule_Lernfeld05_08_Projects/src/main/java/Kaufvertrag/Kaufvertrag/XML/Vertragspartner.xml";
+
   @Override
   public Vertragspartner create()
   {
+    try
+    {
+      Document doc = getDocument(FILEPATH);
+      assert doc != null;
+      Element root = doc.getElementById("vertragspartner");
+      Element nodeID = doc.createElement("id");
+      //get the id here pls.
+      nodeID.setIdAttribute("", true);
+
+      Element vorname = doc.createElement("vorname");
+      vorname.setNodeValue("");
+      nodeID.appendChild(vorname);
+
+      Element nachname = doc.createElement("nachname");
+      nachname.setNodeValue("");
+      nodeID.appendChild(nachname);
+
+      Element ausweisNr = doc.createElement("ausweisNr");
+      ausweisNr.setNodeValue("");
+      nodeID.appendChild(ausweisNr);
+
+      root.appendChild(nodeID);
+      Vertragspartner vertragspartner = new Vertragspartner(vorname.getNodeValue(), nachname.getNodeValue());
+      vertragspartner.setAusweisNr(ausweisNr.getNodeValue());
+      writeToXML(doc, new FileOutputStream(FILEPATH));
+      return vertragspartner;
+    }
+    catch (IOException ex)
+    {
+      System.out.println("There was an unexpected Exception in VertragspartnerDaoXml#create().");
+    }
     return null;
   }
 
   @Override
   public void create(Vertragspartner objectToInsert)
   {
+    try
+    {
+      Document doc = getDocument(FILEPATH);
+      assert doc != null;
+      Element root = doc.getElementById("vertragspartner");
+      Element nodeID = doc.createElement("id");
+      //get the id here pls.
+      nodeID.setIdAttribute("", true);
 
+      Element vorname = doc.createElement("vorname");
+      vorname.setNodeValue(objectToInsert.getVorname());
+      nodeID.appendChild(vorname);
+
+      Element nachname = doc.createElement("nachname");
+      nachname.setNodeValue(objectToInsert.getNachname());
+      nodeID.appendChild(nachname);
+
+      Element ausweisNr = doc.createElement("ausweisNr");
+      ausweisNr.setNodeValue(objectToInsert.getAusweisNr());
+      nodeID.appendChild(ausweisNr);
+
+      root.appendChild(nodeID);
+      writeToXML(doc, new FileOutputStream(FILEPATH));
+    }
+    catch (IOException ex)
+    {
+      System.out.println("There was an unexpected Exception in VertragspartnerDaoXml#create(IVertragspartner objectToInsert).");
+    }
   }
 
   @Override
   public Vertragspartner read(String id)
   {
-    return null;
+    Document doc = getDocument(FILEPATH);
+    assert doc != null;
+    Element root = doc.getElementById("vertragspartner");
+    Element nodeID = root.getOwnerDocument().getElementById(id);
+    Vertragspartner vertragspartner = new Vertragspartner(nodeID.getElementsByTagName("vorname").item(0).getNodeValue(), nodeID.getElementsByTagName("nachname").item(0).getNodeValue());
+    vertragspartner.setAusweisNr(nodeID.getElementsByTagName("ausweisNr").item(0).getNodeValue());
+    return vertragspartner;
   }
 
   @Override
-  public List<Vertragspartner> readAll()
-  {
-    return null;
+  public List<Vertragspartner> readAll() {
+    Document doc = getDocument(FILEPATH);
+    assert doc != null;
+    Element root = doc.getElementById("vertragspartner");
+    List<Vertragspartner> vertragspartnerListe = new ArrayList<>();
+    NodeList vertragspartnerL = root.getElementsByTagName("id");
+    for (int i = 0; i < vertragspartnerL.getLength(); i++) {
+      NodeList children = vertragspartnerL.item(i).getChildNodes();
+      Vertragspartner vertragspartner = new Vertragspartner(children.item(0).getNodeValue(), children.item(1).getNodeValue());
+      vertragspartner.setAusweisNr(children.item(2).getNodeValue());
+      vertragspartnerListe.add(vertragspartner);
+    }
+    return vertragspartnerListe;
   }
 
   @Override
   public void update(Vertragspartner objectToUpdate)
   {
+    try
+    {
+      Document doc = getDocument(FILEPATH);
+      assert doc != null;
+      Element root = doc.getElementById("vertragspartner");
+      //get the id here pls.
+      Element nodeID = root.getOwnerDocument().getElementById("");
 
+      Node vorname = nodeID.getElementsByTagName("vorname").item(0);
+      vorname.setNodeValue(objectToUpdate.getVorname());
+
+      Node nachname = nodeID.getElementsByTagName("nachname").item(0);
+      nachname.setNodeValue(objectToUpdate.getNachname());
+
+      Node ausweisNr = nodeID.getElementsByTagName("ausweisNr").item(0);
+      ausweisNr.setNodeValue(objectToUpdate.getAusweisNr());
+
+      writeToXML(doc, new FileOutputStream(FILEPATH));
+    }
+    catch (IOException ex)
+    {
+      System.out.println("There was an unexpected Exception in VertragspartnerDaoXml#update(IVertragspartner objectToUpdate).");
+    }
   }
 
   @Override
   public void delete(String id)
   {
-
+    try
+    {
+      Document doc = getDocument(FILEPATH);
+      assert doc != null;
+      Element root = doc.getElementById("vertragspartner");
+      Element nodeID = root.getOwnerDocument().getElementById(id);
+      root.removeChild(nodeID);
+      writeToXML(doc, new FileOutputStream(FILEPATH));
+    }
+    catch (IOException ex)
+    {
+      System.out.println("There was an unexpected Exception in VertagspartnerDaoXml#delete(String id).");
+    }
   }
 }
